a4cdb8cb43b96a2ea753010cd3876640e33662c4
amrutadhamgunde <amrutadhamgunde@gmail.com>
2017-03-01 18:06:09 +0530

    Delete HelloWorld.java

--- HelloWorld.java
+++
@@ -1,9 +0,0 @@
-public class HelloWorld{
-
-     public static void main(String args[]){
-        MaxProfitFromAppleStock mpas = new MaxProfitFromAppleStock();
-        int stockPricesYday[] = {80,40,20,50,90,10,100};
-        int max_profit = mpas.getMaxProfit(stockPricesYday);
-        System.out.println("Maximum Profit is :" +max_profit);
-    }
-}
